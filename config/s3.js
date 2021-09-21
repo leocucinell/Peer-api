097934590bf3bc7fcdc0475c5dd98be4957e035c
Leo Cucinell <leocucinell@gmail.com>
2021-09-21 13:56:19 -0700

    fixed issue with undefined .env variables and successfully upload to s3 function

--- config/s3.js
+++ config/s3.js
@@ -1,11 +1,14 @@
 const S3 = require('aws-sdk/clients/s3');
+const fs = require("fs");
+require('dotenv').config();
 
 /* SECTION: Configuration */
 
 const accessId = process.env.ID;
 const secretId = process.env.SECRET_ID;
-const bucketName = process.env.BUCKET;
+const Bucket = process.env.BUCKET;
 const bucketRegion = process.env.AWS_BUCKET_REGION;
+console.log(Bucket)
 
 const s3 = new S3({
     region: bucketRegion,
@@ -16,5 +19,25 @@ const s3 = new S3({
 /* SECTION: Connection methods to AWS-S3 */
 
 //NOTE: Uploads a file to s3
+const uploadToS3 = (file) => {
+    //read the file form the upload (staging) area
+    const fileStream = fs.createReadStream(file.path);
+
+    //confijgure the params to upload to s3
+    console.log(Bucket)
+    const uploadParams = {
+        Bucket,
+        Body: fileStream,
+        Key: file.filename 
+    }
+
+    //upload to s3
+    return s3.upload(uploadParams).promise();
+}
 
 //NOTE: Downloads a file to s3
+
+/* SECTION: Exports */
+module.exports = {
+    uploadToS3,
+}
--- controllers/image_controller.js
+++ controllers/image_controller.js
@@ -1,7 +1,7 @@
 /* SECTION: Modules */
 const { Image } = require("../models");
-const fs = require("fs");
-const AWS = require("aws-sdk");
+const s3 = require("../config/s3");
+
 
 /* SECTION: Middleware */
 
@@ -10,18 +10,26 @@ const AWS = require("aws-sdk");
 const uploadImage = async (req, res, next) => {
     //req.file contains the infomation about the image: Use this to get the file name & store in mongoDB
     //req.body contains the rest of the information about the post req
-    console.log("~~~~~~~~~~~~~~~~");
-    console.log(req.file);
-    console.log(req.body);
-    console.log("~~~~~~~~~~~~~~~~");
-    // try{
-        
+    console.log("inside upload Image function")
+    try{
+        const fileToUpload = await s3.uploadToS3(req.file);
+        console.log("~~~~~~~~~~~~~~~~");
+        console.log(req.file);
+        console.log(fileToUpload);
+        console.log("~~~~~~~~~~~~~~~~");
 
-    // } catch(err){
-    //     return res.status(400).json({
-    //         msg: "unable to upoad image"
-    //     })
-    // }
+        return res.status(200).json({
+            msg: "successfully uploaded file to s3",
+            file: req.file
+        });
+
+    } catch(err) {
+        console.log(err);
+        return res.status(400).json({
+            msg: "unable to upoad image",
+            error: err
+        });
+    }
 
 }
 //get an image
